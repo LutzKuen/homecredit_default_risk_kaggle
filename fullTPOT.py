4dffa8480491935b5a3ff2c9660d0ba78837c9c9
Kuenneke <lutz.kuenneke@bearingpoint.com>
2018-07-14 18:46:03 +0200

    TPOT added

--- fullTPOT.py
+++ fullTPOT.py
@@ -1,4 +1,4 @@
-from tpot impot TPOTRegressor
+from tpot import TPOTRegressor
 import pandas as pd
 
 
@@ -17,19 +17,33 @@ def strToNum(st):
 import pandas as pd
 
 def maskColumn(column, row):
- row[column] = strToNum(row[column])
+ return strToNum(row[column])
+ 
 
 
 
 
 
 if __name__ == '__main__':
- training_file = 'application_train.csv'
- test_file = 'application_test.csv'
- # join bureau
- training = pd.read_csv(training_file)
- test = pd.read_csv(test_file)
- bureau = pd.read_csv('bureau.csv')
- training = pd.merge(training, bureau, how = 'left', on = 'SK_ID_CURR')
- test = pd.merge(test, bureau, how = 'left', on = 'SK_ID_CURR')
- ccbal = pd.read_csv('credit_card_bala
+ dataset = pd.read_csv('feature_matrix.csv')
+ training = dataset[dataset['TARGET'].isnull() == False]
+ test = dataset[dataset['TARGET'].isnull()]
+ target = training['TARGET']
+ training.drop(columns = 'TARGET', axis = 1 , inplace = True)
+ test.drop(columns = 'TARGET', axis = 1 , inplace = True)
+ for column in training.columns: # mask all string values and nan's
+  lfun = lambda row: maskColumn(column, row)
+  training[column] = training.apply(lfun, axis = 1)
+  test[column] = test.apply(lfun, axis = 1)
+ tpot = TPOTRegressor(generations = 5, population_size = 50, verbosity = 2)
+ tpot.fit(training.values, target.values)
+ tpot.export('tpot_homecredit_best_pipeline.py')
+ result = tpot.predict(test.values)
+ submission = zip(test.values[:,0], result[:])
+ submission_file = open('submission.csv','w')
+ submission_file.write('SK_ID_CURR,TARGET\n')
+ sk_id_curr = test['SK_ID_CURR'].values
+ for i in range(len(sk_id_curr)):
+  new_line = str(sk_id_curr[i]) + ',' + str(result[i]) + '\n'
+  submission_file.write(new_line)
+ submission_file.close()
