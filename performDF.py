a8afe8eec8321afb6f3267eada0cf6dd6d9c06cb
Kuenneke <lutz.kuenneke@bearingpoint.com>
2018-07-14 19:28:54 +0200

    added TPOT remote

--- performDF.py
+++ performDF.py
@@ -3,21 +3,21 @@ from AWSTools import createInstance
 cont = createInstance.controller('settings_aws.conf')
 
 cont.createInstance()
-files = [ '/home/ubuntu/external/homecredit/application_train.csv',
-'/home/ubuntu/external/homecredit/application_test.csv',
-'/home/ubuntu/external/homecredit/bureau.csv',
-'/home/ubuntu/external/homecredit/bureau_balance.csv',
-'/home/ubuntu/external/homecredit/POS_CASH_balance.csv',
-'/home/ubuntu/external/homecredit/credit_card_balance.csv',
-'/home/ubuntu/external/homecredit/previous_application.csv',
-'/home/ubuntu/external/homecredit/installments_payments.csv',
-'/home/ubuntu/external/homecredit/df_analysis_kaggle.py']
+files = [ '../data/application_train.csv',
+'../data/application_test.csv',
+'../data/bureau.csv',
+'../data/bureau_balance.csv',
+'../data/POS_CASH_balance.csv',
+'../data/credit_card_balance.csv',
+'../data/previous_application.csv',
+'../data/installments_payments.csv',
+'../data/df_analysis_kaggle.py']
 cont.transferFilesToWorker(files)
 cont.execOnRemote('install_deps.sh')
 scriptName = '/home/ubuntu/external/homecredit/do_df_analysis.sh'
 #cont.execOnRemote('source /home/ubuntu/.bashrc')
 cont.execOnRemote(scriptName)
-resName = ['/home/ubuntu/feature_matrix.csv']
+resName = ['feature_matrix.csv']
 #'/home/ubuntu/feature_descriptions.list']
 cont.retrieveResults(resName)
 cont.terminateInstance()
---
+++ performTPOT.py
@@ -0,0 +1,16 @@
+from AWSTools import createInstance
+
+cont = createInstance.controller('../settings_aws.conf')
+
+cont.createInstance()
+files = [ '../results/feature_matrix.csv',
+'fullTPOT.py']
+cont.transferFilesToWorker(files)
+cont.execOnRemote('install_deps_tpot.sh')
+scriptName = 'do_tpot.sh'
+#cont.execOnRemote('source /home/ubuntu/.bashrc')
+cont.execOnRemote(scriptName)
+resName = ['submission.csv', 'tpot_homecredit_best_pipeline.py']
+#'/home/ubuntu/feature_descriptions.list']
+cont.retrieveResults(resName)
+cont.terminateInstance()
